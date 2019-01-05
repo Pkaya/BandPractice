a047d5427c5414c70b427c49c6b6ba9fcb7fe738
Pkaya <pinar.kaya@students.plymouth.ac.uk>
2019-01-05 03:50:26 +0000

    Fixed deleting users bug changed to object arrays tidied up code

--- public/js/ws-client.js
+++ public/js/ws-client.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-    //test new comment 5
-    var local =  false;
+$(document).ready(function () {
+
+//**********************************************************************************************************************
+// Initiation Code
+//**********************************************************************************************************************
+    var local = true;
     var local_address = 'http://localhost:4000';
     var server_address = 'http://34.220.11.223';
-
-    var address_to_use = local? local_address : server_address;
-
+    var address_to_use = local ? local_address : server_address;
 
     //DOM Login
     var $user_form_area = $("#div_user_form_area");
@@ -20,20 +21,6 @@ $(document).ready(function(){
     var $frm_message = $("#frm_message");
     var $div_lobby_container = $("#div_lobby_container");
 
-    //DOM Instrument
-
-
-
-   //  var arr = [
-   //      { lobby: '1', username: 'test', instrument: 'guitar' },
-   //      { lobby: '1', username: 'gadsf', instrument: 'guitar' } ];
-   //  console.log(arr);
-   //
-   // //arr.splice(arr.indexOf("test"),1);
-   //
-   //  console.log(arr.indexOf("test"));
-
-
 
     //Initially hide the lobby
     $div_lobby_container.hide();
@@ -41,196 +28,253 @@ $(document).ready(function(){
     //Emit Socket events
     var socket = io.connect(address_to_use);
 
-    var drumsounds =
-        [
-            //drums
-            {"char": "q", "filename": "kick"},
-            {"char": "w", "filename": "snare"},
-            {"char": "u", "filename": "bob3"},
-            //guitar
 
-        ];
+    var sounds = {
+        "guitar":[
+            {"char": "a", "filename": "key1"},
+            {"char": "s", "filename": "key2"},
+            {"char": "d", "filename": "key3"},
+            {"char": "f", "filename": "key4"},
+            {"char": "g", "filename": "key5"},
+            {"char": "h", "filename": "key6"},
+            {"char": "j", "filename": "guitar1"},
+            {"char": "k", "filename": "guitar2"},
+            {"char": "l", "filename": "guitar3"},
+            {"char": ";", "filename": "guitar4"},
+            {"char": "'", "filename": "guitar5"}
+        ],
+        "drums":[
+            {"char": "a", "filename": "rim"},
+            {"char": "s", "filename": "snare"},
+            {"char": "d", "filename": "snare2"},
+            {"char": "f", "filename": "kick"},
+            {"char": "g", "filename": "kick2"},
+            {"char": "h", "filename": "bob1"},
+            {"char": "j", "filename": "bob2"},
+            {"char": "k", "filename": "bob3"},
+            {"char": "l", "filename": "bob4"},
+            {"char": ";", "filename": "bob5"},
+        ],
+        "vox":[
+            {"char": "a", "filename": "jyea"},
+            {"char": "s", "filename": "awgh"},
+            {"char": "d", "filename": "hey"},
+            {"char": "f", "filename": "yeauh"},
+            {"char": "g", "filename": "ugh"},
+            {"char": "h", "filename": "aweyeah"},
+            {"char": "j", "filename": "ha"},
+            {"char": "k", "filename": "holdup"},
+            {"char": "l", "filename": "huh"},
+            {"char": ";", "filename": "another"},
+        ]
 
-    var guitarsounds = [
-        {"char": "a", "filename": "key1"},
-        {"char": "s", "filename": "key2"},
-        {"char": "d", "filename": "key3"},
-        {"char": "f", "filename": "key4"},
-        {"char": "g", "filename": "key5"},
-        {"char": "h", "filename": "key6"},
-        {"char": "j", "filename": "guitar1"},
-        {"char": "k", "filename": "guitar2"},
-        {"char": "l", "filename": "guitar3"},
-        {"char": ";", "filename": "guitar4"},
-        {"char": "'", "filename": "guitar5"}
-    ];
+    };
 
 
+//**********************************************************************************************************************
+// Socket Listen / Emit functionality
+//**********************************************************************************************************************
+    // [1] Events which only listen to server
+    // [2] Events which emit to server
+    // [3] Events which listen to server and emit to server
 
 
-    //New User - send username lobby and instrument selected
-    $user_form.on("submit", function (e) {
-        var $sel_instrument_val = $("input[name=optinstrument]:checked").val();
-
-        e.preventDefault();
-        console.log($user_name.val());
-        socket.emit('new_user',{lobby:$lobby_id.val(),username:$user_name.val(),instrument:  $sel_instrument_val}, function(data){
-            //server needs to give the go ahead for username
-            if(data){
-                $user_form_area.hide();
-                $div_lobby_container.show();
-            }
-        });
-
-        switch($sel_instrument_val){
-            case "guitar":
-                buildInstrument(guitarsounds);
-                break;
-            case "drums":
-                buildInstrument(drumsounds);
-        }
-
-    });
-
-    //Message form submitted
-    $frm_message.on("submit", function () {
-        socket.emit('send_message',{message:$txt_message.val(),username:$user_name.val()});
-        $txt_message.val("");
-    });
-
-    //Listen for new message from server
-    socket.on('new_message',function(data){
+    //[1] Listen for new message from server
+    socket.on('new_message', function (data) {
         printMessage(data);
     });
 
-    //Update users
-    socket.on('get_users',function(data){
-        console.log(data);
+    //[1] Update users
+    socket.on('get_users', function (data) {
         var html = '';
-        for(i = 0; i < data.length; i++){
-            html+= '<li >'+data[i].username+'</li>';
-        }
+        loopTrhoughObject(data.users,function(val){
+            html += '<li >' + data.users[val].username + '</li>';
+        });
+
+        // var temp_obj = data.users;
+        // delete temp_obj[data.my_id];
+        //
+        // loopTrhoughObject(temp_obj,function(val){
+        //     var other_instrument = temp_obj[val].instrument;
+        //
+        //     buildInstrument(other_instrument);
+        // });
+
+        /**
+         * Todo - have all instruments built and show when other users are connected
+         */
+        //build other users instruments
+       // buildInstrument(data.instrument);
+
         printToUserList(html);
     });
 
-
-    //Update Instruments
-    socket.on('get_instruments',function(data){
+     //[1] Update instruments to see what's available
+    socket.on('get_instruments', function (data) {
         //disableInstruments();
         //disable and uncheck items in data and default to first other option
-        for (i=0 ; i < data.length; i++){
+        for (i = 0; i < data.length; i++) {
             var instrument = data[i];
-            var target_instr = $(":radio[value="+instrument+"]");
+            var target_instr = $(":radio[value=" + instrument + "]");
             //find radio element
-            if (target_instr !== undefined ){
+            if (target_instr !== undefined) {
                 target_instr.closest('div').hide();
             }
         }
-        console.log(data);
     });
 
+    //[1] Get key from server and play correct sounds
+    socket.on("get_key", function (data) {
+        //console.log(data);
 
-    //Functions and Helper methods
-    function printToUserList(message){
-        $user_list.html(message);
-    }
-
-    //Check server for data format
-    function printMessage(data) {
-        console.log(data);
-        $chat_box.append("<p>"+data.timestamp+":"+"<b>"+data.username+" ></b>"+data.message+"</p>");
-    }
-
-
-
-    //Get Key
-    socket.on("get_key",function(data){
-        console.log(data);
-
-        var filename = "";
-        switch(data.instrument){
-            case "guitar":
-                filename = returnFileName(guitarsounds,data.key);
-                break;
-            case "drums":
-                filename = returnFileName(drumsounds,data.key);
-        }
-
+        var filename = returnFileName(sounds,data.instrument,data.key);
 
         if (filename !== false) {
-            new Audio('/sounds/'+filename + '.mp3').currentTime = 0;
-            new Audio('/sounds/'+filename + '.mp3').play();
+            new Audio('/sounds/'+'/'+data.instrument+'/' + filename + '.mp3').currentTime = 0;
+            new Audio('/sounds/' +'/'+data.instrument+'/' + filename + '.mp3').play();
         }
-
-        console.log(data.filename);
-
-        doBounce($("#div_"+filename), 3, '30px', 50);
+        doBounce($("#div_" + filename), 3, '30px', 50);
     });
 
-
-    console.log(returnFileName(guitarsounds,'a'));
-
-    function returnFileName(array,char) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i].char === char) {
-                return array[i].filename;
-            }
-        }
-        return false;
-    }
-
-
-
-//******************************************************
-// Audio functionality
-//******************************************************
-
+    //[1] Send out keystroke to server
     $(document).keypress(function (event) {
         var keyCode = event.which;
         var keyChar = String.fromCharCode(event.which);
         var $sel_instrument_val = $("input[name=optinstrument]:checked").val();
 
         //Send keycode only if lobby div is visibile and box is active
-        if($div_lobby_container.is(":visible") && $('#instrument_box').hasClass("box-active")){
-            socket.emit('send_key',{instrument: $sel_instrument_val, key:keyChar});
+        if ($div_lobby_container.is(":visible") && $('#instrument_box').hasClass("box-active")) {
+            socket.emit('send_key', {instrument: $sel_instrument_val, key: keyChar});
             console.log(keyCode);
         }
     });
 
+    //[2] Send this new user - send username lobby and instrument selected to server
+    $user_form.on("submit", function (e) {
+        e.preventDefault();
+        var $sel_instrument_val = $("input[name=optinstrument]:checked").val();
 
 
-    $("#instrument_box").on ("click",function(){
+        buildInstruments();
+
+        socket.emit('new_user', {lobby: $lobby_id.val(), username: $user_name.val(), instrument: $sel_instrument_val
+        }, function (data) {
+            //server needs to give the go ahead for username
+            if (data) {
+                $user_form_area.hide();
+                $div_lobby_container.show();
+            }
+        });
+    });
+
+    //[2] Send message when message form submitted
+    $frm_message.on("submit", function () {
+        socket.emit('send_message', {message: $txt_message.val(), username: $user_name.val()});
+        $txt_message.val("");
+    });
+
+//**********************************************************************************************************************
+// //Functions and Helper methods
+//**********************************************************************************************************************
+
+    /**
+     * Apply a function to an object
+     * @param object
+     * @param func
+     */
+    function loopTrhoughObject(object,func){
+        Object.getOwnPropertyNames(object).forEach(func);
+    }
+
+    function printToUserList(message) {
+        $user_list.html(message);
+    }
+
+    /**
+     * Print message from server
+     * @param data
+     */
+    function printMessage(data) {
+        $chat_box.append("<p>" + data.timestamp + ":" + "<b>" + data.username + " ></b>" + data.message + "</p>");
+    }
+
+
+    function returnFileName(object, instrument, char) {
+        var file_name = "";
+
+        loopTrhoughObject(object,function(val,idx){
+            var key_name = Object.keys(object)[idx];
+
+            if (instrument === key_name) {
+
+                for (var i = 0; i < object[val].length; i++) {
+                    //console.log(object[val][i]);
+                    if (object[val][i].char === char) {
+                        //return object[val][i].filename;
+                        //return "yes";
+                        file_name = object[val][i].filename;
+                    }
+                }
+            }
+        });
+        return file_name;
+    }
+
+
+    /**
+     * Build all instruments
+     */
+    function buildInstruments() {
+        var $instrument_box = $("#instrument_box");
+
+        loopTrhoughObject(sounds,function(val,idx){
+            var instr_name = Object.keys(sounds)[idx];
+
+            var instrument_wrapper = '<div class="row row-instrument">';
+            for (var i = 0; i < sounds[val].length; i++) {
+
+                var char = sounds[val][i].char;
+                var filename = sounds[val][i].filename;
+
+                var keydiv = '<div class="col-md-1 audio-box" id="div_' + filename + '">' + filename + '<span>[' + char + ']</span></div>';
+                instrument_wrapper+= (keydiv);
+            }
+            instrument_wrapper+= '</div>';
+
+            $instrument_box.append(instrument_wrapper+'<br><h4>'+instr_name+'</h4>');
+        });
+    }
+
+    /**
+     * Bounce effect for keyboard keys
+     * @param element
+     * @param times
+     * @param distance
+     * @param speed
+     */
+    function doBounce(element, times, distance, speed) {
+        for (var i = 0; i < times; i++) {
+            element.animate({marginTop: '-=' + distance}, speed)
+                .animate({marginTop: '+=' + distance}, speed);
+        }
+    }
+
+
+//******************************************************
+// Temp functionality
+//******************************************************
+
+
+
+    $("#instrument_box").on("click", function () {
         $(this).addClass("box-active");
         $("#chat_box").removeClass("box-active");
     });
 
-    $("#chat_box").on ("click",function(){
+    $("#chat_box").on("click", function () {
         $(this).addClass("box-active");
         $("#instrument_box").removeClass("box-active");
     });
-
-
-
-    function buildInstrument(array){
-        for (var i = 0; i < array.length; i++) {
-            var char = array[i].char;
-            var filename = array[i].filename;
-
-            var keydiv = '<div class="col-md-1 audio-box" id="div_'+filename+'">'+filename+'<span>['+char+']</span></div>';
-            $("#instrument_box").append(keydiv);
-
-        }
-    }
-
-    // new Audio('/sounds/guitar1' + '.mp3').play();
-
-
-    function doBounce(element, times, distance, speed) {
-        for(var i = 0; i < times; i++) {
-            element.animate({marginTop: '-='+distance}, speed)
-                .animate({marginTop: '+='+distance}, speed);
-        }
-    }
 
 
 });//end ready
--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
+//**********************************************************************************************************************
+// Initiation Code
+//**********************************************************************************************************************
+
 const express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 
-const local = false;
+const local = true;
 const dir = __dirname;
 const directory_separator = local ? '\\' : '/';
 const base_directory = dir.substr(0, dir.lastIndexOf(directory_separator));
 
 //if local deploy on 4000 else 80
-var port = local? 4000 : 80;
+var port = local ? 4000 : 80;
 
 
 //Server
@@ -22,70 +26,78 @@ app.use(express.static(base_directory + '/public'));
 
 var socket = require('socket.io');
 var io = socket(server);
-//list of socket connections
-var connections = [];
-var users = [];
-var instruments = [];
 
 
+//Keep track of connections and users
+var connections = {};
+var users = {};
+//var instruments = [];
 
+//**********************************************************************************************************************
+// Socket Listen / Emit functionality
+//**********************************************************************************************************************
 
-io.on('connection',function(socket){
-    connections.push(socket);
-    updateInstruments();
+//Initiate Socket connection
+io.on('connection', function (socket) {
+    //Create random number as no db used
 
-    // var user_ids = Object.keys(SOCKET_LIST);
-    // var num_users = Object.keys(SOCKET_LIST).length+1;
+    var id = Math.floor(Math.random() * 9999) + 1001;
+    socket.id = id;
 
-    //New user - socket.username = data
-    socket.on("new_user",function(data,callback){
+    connections[socket.id] = socket;
+
+    //[3] New user - socket.username = data
+    socket.on("new_user", function (data, callback) {
         callback(true);
-        users.push(data);
-        pre_r(users);
 
-        if(data.instrument!== undefined){
-            instruments.push(data.instrument);
+        users[socket.id] = data;
+
+        //If there is an instrument push it to instruments array
+        if (data.instrument !== undefined) {
+            //instruments.push(data.instrument);
         }
 
-        updateUsernames();
+        updateUsernames(id);
     });
 
 
     //Listen to send_message and emit new_message
-    socket.on("send_message",function(data){
+    socket.on("send_message", function (data) {
         //sent message to every socket
-        io.sockets.emit('new_message', {timestamp:getTimeStamp(),message:data.message,username:data.username});
+        io.sockets.emit('new_message', {timestamp: getTimeStamp(), message: data.message, username: data.username});
     });
 
-    socket.on("send_key",function(data){
-        io.sockets.emit('get_key', {instrument: data.instrument, key:data.key});
+    socket.on("send_key", function (data) {
+        io.sockets.emit('get_key', {instrument: data.instrument, key: data.key});
     });
 
     //Delete users
-    socket.on("disconnect",function(){
-        connections.splice(connections.indexOf(socket),1);
-        console.log(users);
-
-        users.splice(users.indexOf(socket.username),1);
-        instruments.splice(instruments.indexOf(socket.instrument),1);
-        updateUsernames();
-        console.log('Disconnected: %s sockets connected', connections.length )
+    socket.on("disconnect", function () {
+        delete connections[socket.id];
+        delete users[socket.id];
     });
-
-
-    function getTimeStamp(){
-        return new Date().getHours()+':'+ new Date().getMinutes()+ ':'+ new Date().getSeconds()+'';
-    }
-
-    function updateUsernames(){
-        io.sockets.emit('get_users',users);
-    }
-
-    function updateInstruments(){
-        io.sockets.emit('get_instruments',instruments);
-    }
-
+    //instruments.splice(instruments.indexOf(socket.instrument),1);
+    updateUsernames(id);
+    console.log('Disconnected: %s sockets connected', Object.keys(connections).length)
 });
+
+
+function getTimeStamp() {
+    return new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds() + '';
+}
+
+function updateUsernames(id) {
+    io.sockets.emit('get_users', {users: users, my_id:id });
+}
+
+function updateInstruments() {
+    //io.sockets.emit('get_instruments',instruments);
+}
+
+
+//**********************************************************************************************************************
+// Serving and Routing functionality
+//**********************************************************************************************************************
 
 //Set view engine for templating
 app.set('view engine', 'ejs');
@@ -108,18 +120,28 @@ app.get('/', function (req, res) {
 });
 
 //Use template for lobby to dynamically display
-app.get('/lobby/:id',function(req,res){
-    res.render(base_directory+'/views/lobby.ejs',{lobbyId: req.params.id});
+app.get('/lobby/:id', function (req, res) {
+    res.render(base_directory + '/views/lobby.ejs', {lobbyId: req.params.id});
 });
 
 
+function pre_o(object){
+    Object.getOwnPropertyNames(object).forEach(function(val,idx,array){
+        console.log(object[idx]);
+        console.log(object[val]);
+    });
+}
 
 //Debug functions
-function pre_r(array){
+function pre_r(array) {
     var string = "----------------------Array----------------------\n";
 
-   for (i= 0;i <array.length; i++){
-       string+= "["+i+"]"+": "+JSON.stringify(array[i])+"\n";
-   }
-   console.log(string);
+    for (i = 0; i < array.length; i++) {
+        string += "[" + i + "]" + ": " + JSON.stringify(array[i]) + "\n";
+    }
+    console.log(string);
 }
+
+
+
+
